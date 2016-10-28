9ef89adcb8e0d5c854d2e5b8ae8f570eeef43e0a
Kevin Zhu <kevinazhu@gmail.com>
2016-10-27 19:17:21 -0700

    adding event pages and actions. import rails bootstrap now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#landing'
+  get '/test', to: 'static_pages#events'
+  resources :events
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#landing'
-  get '/events', to: 'application#events'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021195136) do
+ActiveRecord::Schema.define(version: 20161028013025) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "location"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
