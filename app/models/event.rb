cd4b9d7f3e68007cd0fec87ec612fda9dd104774
Kevin Zhu <kevinazhu@gmail.com>
2016-11-03 00:49:09 -0700

    cleanup of html. added fixes to destroy participants and comments upon event destruction

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-	has_many :participants
-  has_many :comments
+  has_many :participants, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :user
 end
