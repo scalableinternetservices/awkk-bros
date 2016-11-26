35e567de20bf30fdd669ad7602df19f08e85fb58
Kevin Zhu <kevinazhu@gmail.com>
2016-11-26 02:04:28 -0800

    added server caching for events index page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # temporarily enable caching for development
+  config.action_controller.perform_caching = true
 end
