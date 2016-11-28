f5578b4ad334ddfe59fcebca0a34c305501bf98f
Kevin Zhu <kevinazhu@gmail.com>
2016-11-28 01:59:55 -0800

    added caching for participants and comments

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,13 @@ module EventsHelper
 	def cache_key_for_event_row(event)
     "event-#{event.id}-#{event.updated_at}-#{event.participants.length}"
 	end
+  
+
+  def cache_key_for_event_participants(event)
+    "event-#{event.id}-#{event.updated_at}-#{event.participants.length}-#{event.participants.maximum(:updated_at)}"
+  end
+
+  def cache_key_for_event_comments(event)
+    "event-#{event.id}-#{event.updated_at}-#{event.comments.length}-#{event.comments.maximum(:updated_at)}"
+  end
 end
