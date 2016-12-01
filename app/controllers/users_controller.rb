6fe147f08377d3ac385f6148186c466ef20f8de7
Kevin Zhu <kevinazhu@gmail.com>
2016-12-01 13:27:52 -0800

    added ability to have username in url. added user not found page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,15 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find_by_id(params[:id])
-    @created_events = Event.where(user_id: params[:id])
-    @participating = Participant.where(user_id: params[:id])
+    if @user == nil
+    	@user = User.where(username: params[:id]).first
+	end
+	if @user != nil
+		@created_events = Event.where(user_id: @user.id)
+    	@participating = Participant.where(user_id: @user.id)
+	else
+		@username = params[:id]
+		render :not_found
+	end
   end
 end
